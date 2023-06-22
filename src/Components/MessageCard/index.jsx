263fa76d42d240e398fd3ac93b97624bfae24c53
EmmanuelMadehin <tobimadehin@gmail.com>
2023-06-22 15:28:13 +0000

    Added basic chat functionality

--- src/Components/MessageCard/index.jsx
+++ src/Components/MessageCard/index.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import "./MessageCard.scss";
 import img from "../../door-story.jpg";
 
-const MessageCard = () => {
+const MessageCard = (props) => {
     return (
         <div class="div-message">
             <img src={img} alt="door-story" />
             <div class="div-text">
                 <div class="div-message-info">
-                    <div>Wesley Fox</div>
-                    <div>02:32pm</div>
+                    <div>{props.name}</div>
+                    <div>{props.time}</div>
                 </div>
                 <div class="div-card-message">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    {props.message}
                 </div>
             </div>
-
         </div>
-
     );
 }
 
